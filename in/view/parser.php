13036ff7dc593040be624fe5968f1e905763984d
shriniwas1994 <39135801+shriniwas1994@users.noreply.github.com>
2018-05-10 05:16:35 +0530

    Basic Parsing
    POC achieved

--- in/view/parser.php
+++ in/view/parser.php
@@ -1,5 +1,37 @@
 <?php
-	$cvData = array[
-		Name => "Shriniwas Waphare"
-	];
+	$file = file_get_contents("template.html");
+	echo $file;
+	
+	echo "<br><br>After Parsing<br><br>";
+	
+	$fread = fopen("template.html", "r") or die("can't read stdin");
+	$fwrite = fopen("template2.html", "w") or die("can't write stdin");
+	while (!feof($fread)) {
+		$line = fgets($fread);
+		$line = preg_replace_callback(
+			'|{{(\w*)}}|',
+			"replace",
+			$line
+		);
+		echo $line;
+		fwrite($fwrite, $line);
+	}
+	fclose($fread);
+	fclose($fwrite);
+	
+	function replace($placeHolder) {
+				$cvData = array(
+					"Name" => "Shriniwas Waphare",
+					"Age" => "24",
+					"Sex" => "Male"
+				);
+				
+				$keys = array_keys($cvData);
+				
+				foreach($keys as $key){
+					if($key == $placeHolder[1]){
+						return $cvData[$key];
+					}
+				}
+			}
 ?>
